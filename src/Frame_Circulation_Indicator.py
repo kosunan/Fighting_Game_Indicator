0dc61f4bbe07be471ee0b88655d66255a3f55fdc
kosunan <36727704+kosunan@users.noreply.github.com>
2022-07-13 14:25:34 +0900

    試作

--- src/Frame_Circulation_Indicator.py
+++ src/Frame_Circulation_Indicator.py
@@ -1,90 +1,83 @@
 g_bar_num = 0
-g_bar_flag = 0
-g_bar80_flag = 0
 g_bar_max_flag = 0
-
 g_interval = 0
 g_interval_value = 0
 g_interval_max_flag = 0
-
 g_bar_range = 80
-g_bar_ini_flag = 0
-g_bar_ini_flag_1 = 0
-g_bar_ini_flag_2 = 0
+g_bars_num = 10
 
-g_bar_list_P1["", "", "", "", "", "", "", "", "", ""]
-g_bar_list_P2["", "", "", "", "", "", "", "", "", ""]
-g_bar_list_P3["", "", "", "", "", "", "", "", "", ""]
-g_bar_list_P4["", "", "", "", "", "", "", "", "", ""]
-g_bar_lists = [g_bar_list_P1,
-               g_bar_list_P2,
-               g_bar_list_P3,
-               g_bar_list_P4]
+g_character_elements_bar = list(range(4))
 
-class Element_info:
+for n in range(4):
+    g_character_elements_bar[n] = list(range(10))
+
+g_characters_elements_bar = [g_character_elements_bar[0],
+                             g_character_elements_bar[1],
+                             g_character_elements_bar[2],
+                             g_character_elements_bar[3]
+                             ]
+
+
+class Element_data:
     def __init__(self):
         self.flag = 0
         self.num = 0
         self.line = 0
 
 
-for n in g_bar_list_list:
-    for m in n:
-        m = list(range(bar_range))
-
-    # 4キャラ分＊１０バー取得
-
-
-def Frame_Circulation_Indicator(Character_lists, ini_flag):
+def Frame_Circulation_Indicator(Characters_elements_data, ini_flag, stop_flag):
     global g_bar_num
     global g_bar_max_flag
     global g_interval_value
-    global g_bar_lists
+    global g_characters_elements_bar
 
-    bar_flag = bar_flag_detection(Character_lists):
+    bar_flag = bar_flag_detection(Characters_elements_data)
 
     if ini_flag == 1:  # ラウンド初期化時のバー初期化
         bar_ini()
 
-    if g_interval_max_flag == 1 and bar_flag == 1:  # インターバル経過後かつ表示し始めバー初期化
+    if bar_flag == 1 and g_interval_max_flag == 1:  # インターバル経過後初期化
         bar_ini()
 
     if g_bar_num == g_bar_range:
         g_bar_max_flag = 1
         g_bar_num = 0
 
-    if bar_flag == True:
-        bar_add(Character_lists)
+    if bar_flag == 1:
+        bar_add(Characters_elements_data, stop_flag)
         g_interval_value = 0
 
-    elif bar_flag == False:
+    elif bar_flag == 0:
 
         g_interval_value += 1
 
-        if g_bar_max_flag = 0:  # インターバル問題解決
+        if g_bar_max_flag == 0:
             interval = 80
 
-        elif g_bar_max_flag = 1:  # インターバル問題解決
+        elif g_bar_max_flag == 1:
             interval = 20
 
         if g_interval_value >= interval:  # インターバル問題解決
             g_interval_max_flag = 1
 
         if g_interval_max_flag == 0:
-            bar_add(Character_lists)
+            bar_add(Characters_elements_data, stop_flag)
 
     return g_bar_lists
 
 
-def bar_flag_detection(Character_lists):
-    Character_lists[0]
-    Character_lists[1]
+def bar_flag_detection(Characters_elements_data):
 
-    if info_data.p1.action_flag == 1:
-        return 1
+    p1_elements_data = Characters_elements_data[0]
+    p2_elements_data = Characters_elements_data[1]
 
-    if info_data.p2.action_flag == 1:
-        return 1
+    for element_grain_data in p1_elements_data:
+        if element_grain_data.flag == 1:
+            return 1
+
+    for element_grain_data in p2_elements_data:
+        if element_grain_data.flag == 1:
+            return 1
 
     return 0
 
@@ -93,27 +86,27 @@ def bar_ini():
     global g_bar_max_flag
     global g_bar_num
     global g_interval_value
-    global g_bar_lists
+    global g_characters_elements_bar
 
     g_bar_max_flag = 0
     g_bar_num = 0
     g_interval_value = 0
 
-    for bar_list in g_bar_lists:
-        for bar_grain in bar_list:
-            for i in bar_grain:
-                i = ""
+    for elements_bar in g_characters_elements_bar:
+        for element_bar in elements_bar:
+            for grain in element_bar:
+                grain = ""
 
 
-def bar_add(Character_lists):
-    global g_bar_lists
+def bar_add(characters_elements_data, stop_flag):
+    global g_characters_elements_bar
 
     if stop_flag == 0:
         g_bar_num += 1
 
-    for data, bar_list in zip(Character_lists, g_bar_lists):
+    for elements_data, elements_bar in zip(characters_elements_data, g_characters_elements_bar):
 
-        bar_list[g_bar_num] = create_grain(data)
+        elements_bar[g_bar_num] = create_elements(elements_data)
 
 
 def text_font(rgb):
@@ -130,16 +123,14 @@ def get_font(text_rgb, bg_rgb):
     return text_font(text_rgb) + bg_font(bg_rgb)
 
 
-def create_grain(info_data):
+def create_elements(elements_data):
 
-    for p_data in info_data
+    elements = ["", "", "", "", "", "", "", "", "", ""]
 
-    bar_grain = ["", "", "", "", "", "", "", "", "", ""]
+    for element_data in elements_data:
+        if element_data.flag == 1:
+            font = get_font(element_data.font_coler)
+            num = element_data.num
+            elements[element_data.line] = font + num.rjust(2, " ")[-2:]
 
-    for n in p_data
-    if n.flag == 1:
-        font = get_font(n.font_coler)
-        num = n.num
-        bar_grain[n.line] = font + num.rjust(2, " ")[-2:]
-
-    return bar_grain
+    return elements
